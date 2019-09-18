e74f0e70109cadfd3477baa524635077bf4c5c19
Kirage <tommi.makelainen@tuni.fi>
2019-09-18 13:26:52 +0300

    bomb explosion wip

---
+++ Assets/Scripts/Blast1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blast1 : MonoBehaviour
+{
+    [SerializeField]
+    GameObject fire;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+
+    private float timer;
+
+    [SerializeField]
+    float burnTime = 2.0f;
+
+    [SerializeField]
+    LayerMask destructibleWall;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Debug.Log(destructibleWall.value + "   " + gameObject.layer);
+        timer += Time.deltaTime;
+        if (timer > burnTime)
+        {
+            timer = 0;
+            Destroy(gameObject);
+        }
+    }
+    
+    
+}
