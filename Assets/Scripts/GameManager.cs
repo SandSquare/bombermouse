095bc27a5ce52edbad5440c18b7636fd7561466a
Kirage <tommi.makelainen@tuni.fi>
2019-09-25 12:08:59 +0300

    player movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+using System.Collections.Generic;        //Allows us to use Lists. 
+
+public class GameManager : MonoBehaviour
+{
+    public float levelStartDelay = 2f;                        //Time to wait before starting level, in seconds.
+    public float turnDelay = 0.1f;                            //Delay between each Player turn.
+    public int playerFoodPoints = 100;                        //Starting value for Player food points.
+    public static GameManager instance = null;                //Static instance of GameManager which allows it to be accessed by any other script.
+    [HideInInspector] public bool playersTurn = true;        //Boolean to check if it's players turn, hidden in inspector but public.
+
+
+    private int level = 1;                                    //Current level number, expressed in game as "Day 1".
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    
+        DontDestroyOnLoad(gameObject);
+
+        InitGame();
+    }
+
+    //This is called each time a scene is loaded.
+    void OnLevelWasLoaded(int index)
+    {
+        level++;
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+    }
+
+
+    //Update is called every frame.
+    void Update()
+    {
+    }
+
+
+
+
+    //GameOver is called when the player reaches 0 food points
+    public void GameOver()
+    {
+        enabled = false;
+    }
+
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    IEnumerator m_MoveCoroutine;
+
+    [SerializeField]
+    float m_SpeedFactor;
+
+    int xDir;
+    int yDir;
+    bool legalMove;
+    Vector2 direction;
+
+
+    void Update()
+    {
+        // if character is already moving, just return
+        if (m_MoveCoroutine != null)
+            return;
+
+
+        if (yDir != 0 && xDir == 0)
+        {
+            xDir = (int)Input.GetAxisRaw("Horizontal");
+            yDir = 0;
+        }
+        else if (xDir != 0 && yDir == 0)
+        {
+            yDir = (int)Input.GetAxisRaw("Vertical");
+            xDir = 0;
+        }
+        else
+        {
+            xDir = (int)Input.GetAxisRaw("Horizontal");
+            yDir = (int)Input.GetAxisRaw("Vertical");
+        }
+
+
+        direction = new Vector2(xDir, yDir);
+        legalMove = AttemptMove();
+
+        if (direction != Vector2.zero && legalMove)
+        {
+            // start moving your character
+            m_MoveCoroutine = Move(direction);
+            StartCoroutine(m_MoveCoroutine);
+        }
+    }
+
+    private bool AttemptMove()
+    {
+        bool canMove = true;
+        this.GetComponent<BoxCollider2D>().enabled = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 1f);
+        if (hit.collider != null)
+        {
+            //Debug.Log(hit.collider.gameObject.tag);
+            canMove = false;
+        }
+        this.GetComponent<BoxCollider2D>().enabled = true;
+        return canMove;
+    }
+
+    IEnumerator Move(Vector2 direction)
+    {
+        // Lerp(Vector2 a, Vector2 b, float t);
+        Vector2 orgPos = transform.position; // original position
+        Vector2 newPos = orgPos + direction; // new position after move is done
+        float t = 0; // placeholder to check if we're on the right spot
+        while (t < 1.0f) // loop while player is not in the right spot
+        {
+            // calculate and set new position based on the deltaTime's value
+            transform.position = Vector2.Lerp(orgPos, newPos, (t += Time.deltaTime * m_SpeedFactor));
+            // wait for new frame
+            yield return new WaitForEndOfFrame();
+        }
+        // stop coroutine
+        StopCoroutine(m_MoveCoroutine);
+        // get rid of the reference to enable further movements
+        m_MoveCoroutine = null;
+    }
+}
+
