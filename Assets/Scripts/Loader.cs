8fc456eeaa5e8231a1832bb0e5f344036a4072af
Kirage <tommi.makelainen@tuni.fi>
2019-10-09 14:26:57 +0300

    maincamera prefab

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -7,17 +7,12 @@ public class Loader : MonoBehaviour
 {
     public GameObject gameManager;            //GameManager prefab to instantiate.
 
-    public GameObject splashScreen;
-    //public GameObject soundManager;            //SoundManager prefab to instantiate.
-
 
     void Awake()
     {
 
         if (GameManager.instance == null)
             Instantiate(gameManager);
-        //if (splashScreen == null)
-        //    Instantiate(splashScreen);
 
         //Check if a SoundManager has already been assigned to static variable GameManager.instance or if it's still null
         //if (SoundManager.instance == null)
