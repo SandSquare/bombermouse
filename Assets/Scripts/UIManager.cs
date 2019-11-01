147092990665303e0fd21ce235ac8b92ac2b179c
pasimakit <48199458+pasimakit@users.noreply.github.com>
2019-11-01 15:04:08 +0200

    UI things and player can die

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,57 +10,70 @@ public class UIManager : MonoBehaviour
     public Animator menuPanel;
     //public Animator dialog;
     GameObject inventoryUI;
+    [SerializeField]
+    private GameObject LosePanelUI;
+    [SerializeField]
+    private GameObject WinPanelUI;
 
-    //public static UIManager Instance = null;
+    private int levelToLoad;
 
-    //private void Awake()
-    //{
-    //    // If there is not already an instance of SoundManager, set it to this.
-    //    if (Instance == null)
-    //    {
-    //        Instance = this;
-    //    }
-    //    //If an instance already exists, destroy whatever this object is to enforce the singleton.
-    //    else if (Instance != this)
-    //    {
-    //        Destroy(gameObject);
-    //    }
+    public static UIManager Instance = null;
 
-    //    //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
-    //    DontDestroyOnLoad(gameObject);
-    //}
+    private void Awake()
+    {
+        // If there is not already an instance of SoundManager, set it to this.
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        //If an instance already exists, destroy whatever this object is to enforce the singleton.
+        else if (Instance != this)
+        {
+            Debug.Log("Two UImanager instances");
+            Destroy(gameObject);
+        }
+
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        //DontDestroyOnLoad(gameObject);
+    }
 
     void Start()
     {
         //inventoryUI = this.gameObject.transform.GetChild(0).gameObject;
     }
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene("Level 1");
-    }
-
-    public void QuitGame()
-    {
-        //Debug.Log("lopetus");
-        Application.Quit();
-    }
-
     public void PlayOrPauseMusic()
     {
         SoundManager.Instance.PlayOrPauseMusic();
     }
-         
-    public void MenuButton()
-    {
-        
-    }
 
-    public void GoMainMenu()
+    public void OpenMainMenu()
     {
         SceneManager.LoadScene("MenuScene");
     }
 
+    public void OpenLosePanel()
+    {
+        if (LosePanelUI == null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        LosePanelUI.SetActive(true);
+    }
+
+    public void OpenWinPanel(int level)
+    {
+        level = levelToLoad;
+        if (WinPanelUI == null)
+        {
+            if(level <= 1)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                return;
+            }
+        }
+        WinPanelUI.SetActive(true);
+    }
 
     public void OpenSettings()
     {
@@ -72,9 +85,31 @@ public class UIManager : MonoBehaviour
         menuPanel.SetBool("isHidden", false);
     }
 
+    public void OnNextLevelButton()
+    {
+        if (levelToLoad <= 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+
+        GameManager.instance.LoadNextScene(levelToLoad);
+    }
+
+    public void OnRestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void OnMainMenuButton()
+    {
+        // 0 is MainMenu
+        SceneManager.LoadScene(0);
+    }
+
     public void UpdateUI()
     {
-
+        
     }
 
     public void AddBomb(ObjectColors pickupType)
