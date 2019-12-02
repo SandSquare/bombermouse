a2f4fa121d6acf5df717718aad0b792e35a6f144
Kirage <tommi.makelainen@tuni.fi>
2019-12-02 15:53:27 +0200

    stars in buttons

--- Assets/Scripts/UI/GameLevels.cs
+++ Assets/Scripts/UI/GameLevels.cs
@@ -31,14 +31,22 @@ public class GameLevels : MonoBehaviour
         //EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
         //scenes = scenes.Skip(2).ToArray();
         // Debug.Log("moi "+ scenes.Length);
-        for (int i = 0; i < scenes-2; i++)
+        for (int i = 0; i < scenes - 2; i++)
         {
             //string levelName = Path.GetFileNameWithoutExtension(allFileInfos[i].FullName);
-            string levelName = "Level " + i;
+            string levelName = "Level " + (i + 1);
             GameObject levelBtn = Instantiate(levelButton, Vector3.zero, Quaternion.identity);
             levelBtn.GetComponent<LevelButton>().levelNumber = i + 2;
             levelBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = levelName;
-            levelBtn.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = $"{points[i]} stars";
+            //levelBtn.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = $"{points[i]} stars";
+            GameObject stars = levelBtn.transform.GetChild(1).gameObject;
+            if (points[i] != -1)
+            {
+                for (int j = 0; j < points[i]; j++)
+                {
+                    stars.transform.GetChild(j).gameObject.SetActive(true);
+                }
+            }
             levelBtn.transform.SetParent(this.gameObject.transform);
             Debug.Log("Found a scene file: " + levelName);
             if (points[i] == -1)
