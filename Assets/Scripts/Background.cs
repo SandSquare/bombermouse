411d030d3af9f1a9dce04e879f00f23dc29ba068
Kirage <tommi.makelainen@tuni.fi>
2019-12-04 17:37:39 +0200

    random background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour
+{
+    public Sprite[] images; 
+    void Start()
+    {
+        GetComponent<Image>().sprite = images[(int)Random.Range(0,images.Length)];
+    }
+}
